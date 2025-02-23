5326041aab50f3e28319dbb112c3d4bd6dda01ee
amderbar <amderbar@users.noreply.github.com>
2025-02-23 01:13:18 +0000

    feat: WIP CLIサブコマンドリスト検討

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,45 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main (main) where
 
+import Development.Placeholders (notImplemented)
 import Import
-import Run
-import RIO.Process
 import Options.Applicative.Simple
 import qualified Paths_tomado
+import RIO.Process
+import Run
 
 main :: IO ()
 main = do
-  (options, ()) <- simpleOptions
-    $(simpleVersion Paths_tomado.version)
-    "Header for command line arguments"
-    "Program description, also for command line arguments"
-    (Options
-       <$> switch ( long "verbose"
-                 <> short 'v'
-                 <> help "Verbose output?"
-                  )
-    )
-    empty
+  (options, ()) <-
+    simpleOptions
+      $(simpleVersion Paths_tomado.version)
+      "Header for command line arguments"
+      "Program description, also for command line arguments"
+      ( Options
+          <$> switch
+            ( long "verbose"
+                <> short 'v'
+                <> help "Verbose output?"
+            )
+      )
+      subcommands
   lo <- logOptionsHandle stderr (optionsVerbose options)
   pc <- mkDefaultProcessContext
   withLogFunc lo $ \lf ->
-    let app = App
-          { appLogFunc = lf
-          , appProcessContext = pc
-          , appOptions = options
-          }
+    let app =
+          App
+            { appLogFunc = lf,
+              appProcessContext = pc,
+              appOptions = options
+            }
      in runRIO app run
+  where
+    subcommands = do
+      addCommand "init" "setup TomaDo working space" $notImplemented (pure ())
+      addCommand "config" "display or edit TomaDo configuration" $notImplemented (pure ())
+      addCommand "list" "display a todo list" $notImplemented (pure ())
+      addCommand "today" "display today's todo list" $notImplemented (pure ())
+      addCommand "new" "make a new todo" $notImplemented (pure ())
+      addCommand "done" "make a todo done" $notImplemented (pure ())
